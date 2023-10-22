598bef5c0a1afda41d581e3350d64ffe666a1fe7
Seva <vsevolod@reffie.me>
2023-10-23 01:46:09 +0300

    Upgrade main window

---
+++ app/src/main/java/com/example/mathematics/CombinatoricsView.kt
@@ -0,0 +1,11 @@
+package com.example.mathematics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CombinatoricsView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_combinatorics_view)
+    }
+}
